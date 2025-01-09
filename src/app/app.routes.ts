f6c1bfd320f7bb54cdb5978ee69eee18ddb26863
Charles Schaefer <charlesschaefer@gmail.com>
2025-01-09 20:04:26 -0300

    Adding license

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,4 +3,5 @@ import { MainComponent } from './main/main.component';
 
 export const routes: Routes = [
     { path: '', component: MainComponent },
+    { path: "license", loadComponent: () => import('./license/license.component').then(comp => comp.LicenseComponent) },
 ];
