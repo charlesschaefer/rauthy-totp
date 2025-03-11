5a4f42c460f79c621149809ae9db416f0c940942
Charles Schaefer <charlesschaefer@gmail.com>
2025-03-11 06:53:34 -0300

    Adding the service search feature

--- src/app/main/service-list/service-list.component.ts
+++ src/app/main/service-list/service-list.component.ts
@@ -1,10 +1,12 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, computed, effect, EventEmitter, input, Input, Output, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslocoModule } from '@jsverse/transloco';
 import { ButtonModule } from 'primeng/button';
 import { AvatarModule } from 'primeng/avatar';
 import { KnobModule } from 'primeng/knob';
 import { RippleModule } from 'primeng/ripple';
+import { InputTextModule } from 'primeng/inputtext';
+import { AutoFocusModule } from 'primeng/autofocus';
 import { NgxSwipeMenuComponent, SwipeMenuActions } from 'ngx-swipe-menu';
 
 import { Service } from '../../models/service.model';
@@ -22,13 +24,16 @@ import { FormsModule } from '@angular/forms';
         AvatarModule,
         KnobModule,
         RippleModule,
-        NgxSwipeMenuComponent
+        InputTextModule,
+        AutoFocusModule,
+        NgxSwipeMenuComponent,
     ],
     templateUrl: './service-list.component.html',
     styleUrl: './service-list.component.scss'
 })
 export class ServiceListComponent {
-    @Input() totpItems = new Map<string, Service>();
+    // @Input() totpItems = new Map<string, Service>();
+    totpItems = input<Map<string, Service>>(new Map<string, Service>());
     @Output() totpItemsChange = new EventEmitter<Map<string, Service>>();
     @Input() tokensMap = new Map<string, TotpToken>();
     @Input() tokensDuration = new Map<string, number>();
@@ -37,12 +42,15 @@ export class ServiceListComponent {
     @Output() editService = new EventEmitter<Service>();
     @Output() deleteService = new EventEmitter<Service>();
 
+    itemList = computed(() => this.filter(this.totpItems().values()));//Array.from(this.totpItems.values());
+
+    searchFilter = signal('');
+
     actionList = [
         {
             name: 'edit',
             label: 'Edit',
             class: '',
-            //data: 'treta',
             onClick: (_event: any, data: any) => {
                 this.editService.emit(data);
             }
@@ -57,27 +65,35 @@ export class ServiceListComponent {
         }
     ] as SwipeMenuActions[];
 
+    constructor() {}
+
     onSwipeLeft(service: Service) {
-        console.log("Swipe left")
         this.editService.emit(service);
     }
 
     onSwipeRight(service: Service) {
-        console.log("Swipe right")
         this.deleteService.emit(service);
     }
 
     onImageError(event: any, service: Service) {
         console.error("Couldn't load service logo at: ", event.srcElement?.currentSrc);
         
-        for (let item of this.totpItems) {
+        for (let item of this.totpItems()) {
             if (item[1].id == service.id) {
                 item[1].icon = "";
-                this.totpItems.set(item[0], item[1]);
+                this.totpItems().set(item[0], item[1]);
                 break;
             }
         }
 
-        this.totpItemsChange.emit(this.totpItems);
+        this.totpItemsChange.emit(this.totpItems());
+    }
+
+    private filter(items: IterableIterator<Service>) {
+        return Array.from(items)
+            .filter((service) => {
+                return this.searchFilter() === "" ||
+                    (service.issuer.includes(this.searchFilter()) || service.name.includes(this.searchFilter()));
+            });
     }
 }
